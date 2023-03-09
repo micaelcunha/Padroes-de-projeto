5976bf1fb135f8e37f86d6d756171d6b3fdcf7b4
Micael Cunha <110691440+micaelcunha@users.noreply.github.com>
2023-03-08 21:25:26 -0300

    rat_commit

--- [Aula 15] Observer/GameOfRatsSENAI/rat.py
+++ [Aula 15] Observer/GameOfRatsSENAI/rat.py
@@ -5,7 +5,12 @@ class Rat:
     def __init__(self, game: Game):
         self.game = game
         self.attack: int = 1
+        game.rat_enters.append(self.rat_enters)
+        game.notify_rat.append(self.notify_rat)
 
+        self.game.rat_enters(self)
+        
+        
     def rat_enters(self, which_rat):
         pass
 
