a3a85fe7cc288ae1aa23f8aa4f174ce55e0ab514
Micael Cunha <micaaelvitoor2015@gmail.com>
2023-03-10 10:26:19 -0300

    terminei_Observer

--- [Aula 15] Observer/GameOfRatsSENAI/main.py
+++ [Aula 15] Observer/GameOfRatsSENAI/main.py
@@ -26,9 +26,23 @@ def test_three_rats():
     print(f"rat 2: {rat2.attack} should equal 3.")
     print(f"rat 3: {rat3.attack} should equal 3.")
 
+def test_for_rats():
+    game = Game()
+    rat = Rat(game, "rato1")
+    rat2 = Rat(game, "rato2")
+    rat3 = Rat(game, "rato3")
+    rat4 = Rat(game, "rato4")
+
+    print(f"rat 1: {rat.attack} should equal 4.")
+    print(f"rat 2: {rat2.attack} should equal 4.")
+    print(f"rat 3: {rat3.attack} should equal 4.")
+    print(f"rat 4: {rat4.attack} should equal 4.")
+
 
 test_single_rat()
 print("\n")
 test_two_rats()
 print("\n")
 test_three_rats()
+print("\n")
+test_for_rats()
--- [Aula 15] Observer/GameOfRatsSENAI/rat.py
+++ [Aula 15] Observer/GameOfRatsSENAI/rat.py
@@ -6,7 +6,7 @@ class Rat:
         self.name = name
         self.game = game
         self.attack: int = 1
-        print(f"instanciei o {self.name}")
+        #print(f"instanciei o {self.name}")
         game.rat_enters.append(self.rat_enters)
         game.notify_rat.append(self.notify_rat)
 
@@ -14,57 +14,25 @@ class Rat:
 
     def rat_enters(self, which_rat):
         if self == which_rat:
-            print(f"rat_enters --- {self.name} é igual {which_rat.name}")
-            print("Um rato entrou no game")
+            #print(f"rat_enters --- {self.name} é igual {which_rat.name}")
+            if which_rat.attack == 1:
+                #print("Entrou o primeiro rato no game")
+                return
         else:
-            print(f"rat_enters --- {self.name} não é igual {which_rat.name}")
-            print("entrou um novo rato no game")
-            which_rat.attack = self.attack + 1
-            self.game.notify_rat(which_rat)
+            #print(f"rat_enters --- {self.name} não é igual {which_rat.name}")
+            if which_rat.attack == 1:
+                #print("entrou um novo rato no game")
+                self.game.notify_rat(which_rat)
 
     def notify_rat(self, which_rat):
         if self == which_rat:
-            print(f"--- notify_rat --- {self.name} é igual {which_rat.name}")
-            #which_rat.attack += 1
-            #print(f"{which_rat.name}: attack +1")
-        else:
-            print(f"--- notify_rat --- {self.name} não é igual {which_rat.name}")
-            self.attack = which_rat.attack
+            #print(f"--- notify_rat --- {self.name} é igual {which_rat.name}")
             return
-            #print(f"{self.name}: attack +1")
+        
+        #print(f"--- notify_rat --- {self.name} não é igual {which_rat.name}")
+        self.attack += 1
+        #print(f"o {self.name}: attack +1")
+        which_rat.attack = self.attack
+        #print(f"o {which_rat.name}: = {self.attack}")
         
         
-        
-    #def rat_enters(self, which_rat):
-     #   print("entrou um rato")
-      #  self.notify_rat(which_rat)
-       # self.game.rat_enters.remove(self.rat_enters)
-
-    #def notify_rat(self, which_rat):
-     #   which_rat.attack
-      #  self.game.notify_rat.remove(self.notify_rat)
-
-
-
-
-
-
-
-
-
-    #def __init__(self, observer: Observer) -> None:
-        #self.observer = observer
-        #self.attack: int = 1
-        #self.adicionar_rato()
-
-
-
-    #def adicionar_rato(self):
-     #   self.observer.rats_observers.append(self)
-      #  print("um rato entrou")
-       # self.notificar()
-        #self.observer.rats_notify.append(self)
-
-    #def notificar(self):
-     #   for rat in self.observer.rats_notify:
-      #      rat.attack += 1
